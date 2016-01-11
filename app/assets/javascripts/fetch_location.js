e2e22e070bd99b711d77d6b67d8fb23ffae2ebd7
dancinghoneybees <opheliahl@hotmail.com>
2016-01-10 21:39:59 -0500

    add javascript files for google maps api

---
+++ app/assets/javascripts/fetch_location.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#go_country').on('submit', function(e){
+    var country = $('#country_name').val();
+  });
+});
---
+++ app/assets/javascripts/insert_map.js
@@ -0,0 +1,8 @@
+function insertMap(latitude, longitude) {
+  var mapCanvas = document.getElementById('map_canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(latitude, longitude),
+      zoom: 10, mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+};
